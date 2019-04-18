4817c07b69a129a319a37a82cba82ea8530d832d
mathematical-algorithm-3478 <jlboyer1@gmail.com>
2019-04-18 18:09:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function happyHolidayTo(holiday,person){
   return(`Happy ${holiday}, ${person}!`)
 };
 
-function holidayCountdown(days, holiday){
+function holidayCountdown(holiday, days){
   return(`It's ${days} days until ${holiday}`)
 };
